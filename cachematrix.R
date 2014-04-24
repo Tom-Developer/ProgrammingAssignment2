fd70075a8316ca65567a40639a4fea41ea4f4c40
Tom Parpura <tomp.developer@gmail.com>
2014-04-23 23:44:40 -0400

    Code with comments. Final assignment version.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions calculating a matrix inverse and storing
+## it in a memory cache for later use.
 
-## Write a short comment describing this function
-
+## This function defines a series of primitive function objects
+## which set and return cached matrix inverse. The function objects
+## are stored in a list which is returned to the caller. This list
+## can be used to invoke individual functions in a different context.
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -18,8 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## This function either calculates the given matrix inverse or
+## if such exists, pulls it from cache (defined by makeCacheMatrix())
+## and returns it to the caller.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
